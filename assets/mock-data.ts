e2bb933adaff5f69c8147edefadaf8a96759fc22
Tushar Zol <tushar.zol@calsoftinc.com>
2020-08-19 01:26:12 +0530

    Auto-generated commit

---
+++ assets/mock-data.ts
@@ -0,0 +1,38 @@
+export const AddressBook = [
+    {
+      contactId: 1,
+      firstName: 'MS',
+      lastName: 'Dhoni',
+      contactType: 'personal',
+      contactNo: ['+911111111111', '+912222222222'],
+      email: ['ms@gmail.com', 'dhoni@gmail.com'],
+      birthDate: '1990-10-21'
+    },
+    {
+        contactId: 2,
+        firstName: 'Virat',
+        lastName: 'Kohli',
+        contactType: 'office',
+        contactNo: ['+913333333333', '+914444444444'],
+        email: ['virat@gmail.com', 'kohli@gmail.com'],
+        birthDate: '1994-11-18'
+    },
+    {
+        contactId: 3,
+        firstName: 'Yuvraj',
+        lastName: 'Singh',
+        contactType: 'home',
+        contactNo: ['+915555555555', '+916666666666'],
+        email: ['yuvraj@gmail.com', 'singh@gmail.com'],
+        birthDate: '1991-06-26'
+    },
+    {
+        contactId: 4,
+        firstName: 'Rohit',
+        lastName: 'Sharma',
+        contactType: 'personal',
+        contactNo: ['+917777777777', '+918888888888'],
+        email: ['rohit@gmail.com', 'sharma@gmail.com'],
+        birthDate: '1991-01-02'
+    }
+];
